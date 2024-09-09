ad13a26b14f5da80ae119c3f0c320b5b5e59368f
thiagopdo <thiago.falconer@gmail.com>
2024-09-08 23:10:40 -0300

    feat: add global styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <div>Hello world</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  );
 }
