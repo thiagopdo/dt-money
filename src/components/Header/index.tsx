bdf3f4a7a43f2b4d840c1fac0e254c92d98989b9
thiagopdo <thiago.falconer@gmail.com>
2024-09-11 13:04:15 -0300

    feat: added motal for transaction and styled it

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Triangle } from "phosphor-react";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export default function Header() {
   return (
@@ -9,7 +10,10 @@ export default function Header() {
         <Triangle size={20} weight="fill" color="#00875f" />
 
         <Dialog.Root>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
