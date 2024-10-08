12f881b06f9f44d23655425cd29f8505f58f79f0
thiagopdo <thiago.falconer@gmail.com>
2024-10-08 18:02:17 -0300

    feat: added search functionality

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./style";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../context/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,6 +13,7 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
   const {
     register,
     handleSubmit,
@@ -19,8 +22,8 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   });
 
-  function handleSearchTransactions(data: SearchFormInputs) {
-    console.log(data);
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query);
   }
 
   return (
--- src/context/TransactionsContext.tsx
+++ src/context/TransactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -22,18 +23,27 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  useEffect(() => {
-    async function fetchTransactions() {
-      const response = await fetch("http://localhost:3000/transactions");
-      const data = await response.json();
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
 
-      setTransactions(data);
+    if (query) {
+      url.searchParams.append("q", query);
     }
+
+    console.log(url.toString());
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
     fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
